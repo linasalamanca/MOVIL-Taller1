61d63221405dfde672b2f700b8c7124742678df2
linasalamanca <118871999+linasalamanca@users.noreply.github.com>
2024-02-25 20:42:49 -0500

    favs

--- app/src/main/java/com/example/taller1/Favoritos.kt
+++ app/src/main/java/com/example/taller1/Favoritos.kt
@@ -7,15 +7,21 @@ import android.widget.Button
 import android.widget.ListView
 
 class Favoritos : AppCompatActivity() {
+    lateinit var arregloFav: MutableList<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_favoritos)
+        setContentView(R.layout.favoritoss)
 
         val BTNVolver = findViewById<Button>(R.id.btnVolver)
         BTNVolver.setOnClickListener{
             val intentMenu = Intent(this, MainActivity::class.java)
             startActivity(intentMenu)
         }
+        val listaFav = findViewById<ListView>(R.id.listaFav)
+        arregloFav = mutableListOf()
+
+        val favs = MainActivity.favoritos;
+
 
     }
 }
